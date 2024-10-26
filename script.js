1860635508f93f66aa051c1a21136d02a8ec31c0
joperopi <70579397+joperopi@users.noreply.github.com>
2024-10-26 11:13:28 -0300

    Add .join to secondhalf

--- script.js
+++ script.js
@@ -37,42 +37,49 @@ operations.addEventListener("click", function(e) {
         let operationChoice = e.target.innerText; //operationChoice is latest operation to be pressed
         if (values.length === 0 && !operationChoice === "=") {
             values.push(0, operationChoice); // if theres nothing at the time of pressing
+            console.log("here");
         } else if (
             values[1] === "+" ||
             values[1] === "-" ||
             values[1] === "×" ||
             values[1] === "÷" ) {
-                let secondHalf = values.splice(2);
-                console.log("second half ",secondHalf)
+                let secondHalf = values.splice(2).join("");
                 if (secondHalf.length === 0) {
                     values[1] = operationChoice;
+                    console.log("here");
                 } else {
                     let first = Number(values[0]);
                     let second = Number(secondHalf);
                     if (values[1] === "+") {
                         values.splice(0);
                         values.push(add(first, second), operationChoice);
+                        console.log("here");
                     } else if (values[1] === "-") {
                         values.splice(0);
                         values.push(subtract(first, second), operationChoice);
+                        console.log("here");
                     } else if (values[1] === "×") {
                         values.splice(0);
                         values.push(multiply(first, second), operationChoice);
+                        console.log("here");
                     } else if (values[1] === "÷") {
                         values.splice(0);
                         values.push(divide(first, second), operationChoice);
+                        console.log("here");
                         if (values[0] === "ERROR") {
                             values.pop();
                         }
                     }
-                }
+                }  
         } else {
             let firstHalf = values.reduce((a, b) => a.toString() + b.toString());
             values.splice(0);
             if (operationChoice === "="){
                 values.push(firstHalf);
+                console.log("here");
             }else {
                 values.push(firstHalf,operationChoice);
+                console.log("here");
             }
         }
         displayMain.innerText = values.join("");
